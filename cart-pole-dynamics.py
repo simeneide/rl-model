18a510fda86e4ecf38daef390ab93cbd1d184336
Simen <simen@eide.ai>
2019-11-26 14:06:12 +0100

    improved cartpole-dynamics

--- cart-pole-dynamics.py
+++ cart-pole-dynamics.py
@@ -5,29 +5,43 @@ import torch.nn as nn
 import utils
 
 #%%
-data = utils.ReplayBufferTorch(capacity = 1000, obs_dim = (4,))
+obs_len = 2
+
+data = utils.ReplayBufferTorch(capacity = 1000, obs_dim = (obs_len,))
 dl = torch.utils.data.DataLoader(data, batch_size = 50)
 
 #%% play game
 
-num_episode = 100
-num_max_steps = 100
-env = gym.make('CartPole-v0')
 
-for i_episode in range(num_episode):
-    obs = env.reset()
-    episode_data = []
-    for t in range(num_max_steps):
-        action = env.action_space.sample()
-        obs_next, reward, done, info = env.step(action)
-        data = {'obs' : obs,'action' :  action, 'obs_next' :  obs_next, 'reward' : reward, 'done' : done}
-        episode_data.append(data)
-        dl.dataset.push(data)
-        obs = obs_next
-        if done:
-            #print(f"Episode finished after {format(t+1)} timesteps")
-            break
-env.close()
+class Game:
+    def __init__(self):
+        self.env = gym.make('MountainCar-v0')
+        self.num_max_steps = 1000
+    def play_game(self, render=False):
+        obs = self.env.reset()
+        episode_data = []
+        for t in range(self.num_max_steps):
+            if render:
+                self.env.render()
+            action = self.env.action_space.sample()
+            obs_next, reward, done, info = self.env.step(action)
+            data = {'obs' : obs, 'action' :  action, 'obs_next' :  obs_next, 'reward' : reward, 'done' : done}
+            episode_data.append(data)
+            dl.dataset.push(data)
+            obs = obs_next
+            if done:
+                #print(f"Episode finished after {format(t+1)} timesteps")
+                break
+        return episode_data
+
+    def play_many_games(self):
+        num_episode = 100
+
+        for i_episode in range(num_episode):
+            self.play_game()
+game = Game()
+game.play_many_games()
+
 
 #%%
 import torch.distributions.constraints as constraints
@@ -41,21 +55,23 @@ from pyro.optim import Adam
 class Network(nn.Module):
     def __init__(self, obs_dim, action_dim = (1,), seed = 1, device="cpu"):
         super(Network, self).__init__()
-        h_num = 100
+        h_num = 3
+        self.actionemb = nn.Embedding(3, h_num)
         self.enc_obs = nn.Linear(obs_dim[0], h_num)
         self.enc_action = nn.Linear(1,h_num)
+        self.h1 = nn.Linear(h_num, h_num)
         self.head_mean = nn.Linear(h_num, obs_dim[0])
         #self.head_scale = nn.Linear(obs_dim[0] + action_dim[0], obs_dim[0])
 
     def forward(self, batch):
-        obs_encoded = torch.tanh(self.enc_obs(batch['obs']))
-        action_encoded = torch.tanh(self.enc_action(batch['action']))
+        obs_encoded = torch.relu(self.enc_obs(batch['obs']))
+        action_encoded = self.actionemb(batch['action'].long())
 
         x = obs_encoded + action_encoded
-
+        #x = torch.relu(self.h1(x))
         mu = self.head_mean(x)
         #scale = self.head_scale(x)
-        return mu#, scale
+        return mu
 
 class StateTransitionModel:
     def __init__(self, obs_dim, action_dim = (1,), seed = 1, device="cpu"):
@@ -87,7 +103,7 @@ class StateTransitionModel:
         return net
 
     def init_opt(self):
-        adam_params = {"lr": 0.005, "betas": (0.90, 0.999)}
+        adam_params = {"lr": 0.0001, "betas": (0.90, 0.999)}
         optimizer = Adam(adam_params)
 
         self.svi = SVI(self.model, self.guide, optimizer, loss=Trace_ELBO())
@@ -103,13 +119,13 @@ class StateTransitionModel:
             totloss+= loss
         return totloss/i
 
-system = StateTransitionModel(obs_dim = (4,))
+system = StateTransitionModel(obs_dim = (obs_len,))
 pyro.clear_param_store()
 
 #%% TRAIN DYNAMICS
 
 L = []
-for _ in range(200):
+for _ in range(50):
     loss = system.train_epoch(dl)
     L.append(loss)
 
@@ -119,36 +135,38 @@ plt.yscale("log")
 
 #%%
 import matplotlib.pyplot as plt
-for i in range(4):
+for i in range(obs_len):
     plt.plot(dl.dataset.data['obs_next'][:,i], color = "green")
-    for _ in range(100):
-        pred_next_obs = system.guide(None)({'obs' : dl.dataset.data['obs'], 'action' : dl.dataset.data['action']})
-        plt.plot(pred_next_obs.detach()[:,i], alpha = 0.05)
+    for _ in range(50):
+        pred_next_obs = system.guide(None)({'obs' : dl.dataset.data['obs'], 'action' : dl.dataset.data['action'].squeeze()})
+        plt.plot((pred_next_obs.detach()[:,i]), alpha = 0.05)
     plt.show()
 
 #%% PREDICT FROM START
-num_traces = 100
+num_traces = 50
+episode_data = game.play_game(render=False)
 num_timesteps = len(episode_data)
-obs_len = 4
 traces = [system.guide(None) for _ in range(num_traces)]
 pred_states = torch.zeros((num_traces, num_timesteps, obs_len))
 obs_states = torch.zeros((1, num_timesteps, obs_len))
 
 for k, net in enumerate(traces):
     for t, dat in enumerate(episode_data[:-1]):
-        if (t == 0) | (t==10):
+        if (t == 0) | (t==100):
             pred_states[k,t,:] = torch.tensor(dat['obs']).unsqueeze(0).float()
         obs_states[0,t,:] = torch.tensor(dat['obs']).unsqueeze(0).float()
         pred_states[k,t+1,:] = net(
             {'obs' : pred_states[k,t,:], 'action' : torch.tensor(dat['action']).float().unsqueeze(0)}
             )
 
-#%% PLOT TRAJECTORIES
-for i in range(4):
+# PLOT TRAJECTORIES
+for i in range(obs_len):
     plt.plot(obs_states[0,:,i], color = "green")
-    plt.plot(pred_states.detach()[:,:,i].mean(0), color = "red")
+    plt.plot(pred_states.detach()[:,:,i].mean(0), color = "red", alpha = 0.5)
     for k in range(num_traces):
         plt.plot(pred_states.detach()[k,:,i], alpha = 5/num_traces)
+    
+    plt.ylim(obs_states[0,:,i].min()*2,obs_states[0,:,i].max()*2)
     plt.show()
 
 # %% LEARN STATE DYNAMICS
